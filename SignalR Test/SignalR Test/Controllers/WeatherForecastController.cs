517dad063eccd0d08e194453a70141414dabf6d4
EPAM\Ankit_Soni <ankit_Soni@epam.com>
2022-01-08 10:06:22 +0530

    Initial Signal R

---
+++ SignalR Test/SignalR Test/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using SignalR_Test.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SignalR_Test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IHubContext<ChatHub> hubcontext;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IHubContext<ChatHub> hubcontext)
+        {
+            _logger = logger;
+            this.hubcontext = hubcontext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            ChatHub chatHub = new ChatHub();
+            var rng = new Random();
+            for (int i = 0; i <= 100; i++) {
+                await hubcontext.Clients.All.SendAsync("ReceiveMessage", "Ankit",i);
+                Thread.Sleep(300);
+            }
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
